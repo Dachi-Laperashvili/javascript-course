72c261b8ba132f12d9d185987f4592de114b5de3
Dachi <dachi.laperashvili@gmail.com>
2022-06-29 23:54:24 +0400

    completed coding challenge #2

--- JavaScript Fundamentals – Part 2/coding challenges/challenges.js
+++ JavaScript Fundamentals – Part 2/coding challenges/challenges.js
@@ -20,3 +20,23 @@ function checkWinner(avgDolphins, avgKoalas) {
     }
 };
 console.log(checkWinner(scoreDolphins, scoreKoalas));
+
+// coding challenge #2
+
+function calcTip(bill) {
+    return bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
+}
+console.log(calcTip(100));
+
+let bills = [125, 555, 44];
+let tips = [
+    calcTip(bills[0]),
+    calcTip(bills[1]),
+    calcTip(bills[2])
+];
+
+const total = [bills[0] + tips[0], bills[1] + tips[1], bills[2] + tips[2]];
+
+console.log(bills);
+console.log(tips);
+console.log(total);
