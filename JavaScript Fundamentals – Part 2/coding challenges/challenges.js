be9995ea1722d3413d084861baef05787b7c977c
Dachi <dachi.laperashvili@gmail.com>
2022-06-30 23:04:13 +0400

    fixed name spelling errors

--- JavaScript Fundamentals – Part 2/coding challenges/challenges.js
+++ JavaScript Fundamentals – Part 2/coding challenges/challenges.js
@@ -46,9 +46,9 @@ const mark = {
     fullName: 'Mark Miller',
     mass: 78,
     height: 1.69,
-    clcBMI: function () {
-        this.BMI = this.mass / this.height ** 2;
-        return this.BMI;
+    calcBMI: function () {
+        this.bmi = this.mass / this.height ** 2;
+        return this.bmi;
     }
 };
 
@@ -57,16 +57,17 @@ const john = {
     fullName: 'John Smith',
     mass: 92,
     height: 1.95,
-    clcBMI: function () {
-        this.BMI = this.mass / this.height ** 2;
-        return this.BMI;
+    calcBMI: function () {
+        this.bmi = this.mass / this.height ** 2;
+        return this.bmi;
     }
 };
-
+mark.calcBMI();
+john.calcBMI();
 console.log(mark.bmi, john.bmi);
 
 if (mark.bmi > john.bmi) {
-    console.log(`${mark.fullName}'s BMI (${mark.bmi}) is higher than ${john.fullName}'s (${john.bmi})`);
+    console.log(`${mark.fullName}'s BMI (${mark.bmi}) is higher than ${john.fullName}'s BMI (${john.bmi})`)
 } else if (john.bmi > mark.bmi) {
     console.log(`${john.fullName}'s BMI (${john.bmi}) is higher than ${mark.fullName}'s (${mark.bmi})`);
 }
