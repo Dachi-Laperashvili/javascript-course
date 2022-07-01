1fb3e541ddf622adc2cb44fe4a6ba441d9935fdd
Dachi <dachi.laperashvili@gmail.com>
2022-07-01 14:57:13 +0400

    did basic loops with arrays

--- JavaScript Fundamentals – Part 2/script.js
+++ JavaScript Fundamentals – Part 2/script.js
@@ -256,3 +256,33 @@ console.log(myCountry.checkIsland());
 for (let voter = 1; voter <= 50; voter++) {
     console.log(`Voter number ${voter} is currently voting!`);
 }
+
+// Looping arrays
+
+const jonas = [
+    'Jonas',
+    'Schmedtmann',
+    2022 - 1991,
+    'teacher',
+    ['Michael', 'Peter', 'Steven'],
+    true
+];
+
+const types = [];
+
+for (let i = 0; i < jonas.length; i++) {
+    console.log(jonas[i]);
+
+    // types[i] = typeof jonas[i];
+    types.push(typeof jonas[i]);
+}
+
+console.log(types);
+
+const years = [1991, 2007, 1969, 2020];
+const ages = [];
+
+for (let i = 0; i < years.length; i++) {
+    ages.push(2022 - years[i]);
+}
+console.log(ages);
