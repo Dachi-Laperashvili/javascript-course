983d15a6c2823514c6a46ac180d160a862b68b35
Dachi <dachi.laperashvili@gmail.com>
2022-07-03 12:12:56 +0400

    did some basic DOM manipulation

--- 5 Guess my number/script.js
+++ 5 Guess my number/script.js
@@ -1,3 +1,13 @@
 'use strict';
 
 console.log(document.querySelector('.message').textContent);
+
+let message = (document.querySelector('.message').textContent =
+  '🎉 Correct Number! ');
+console.log(message);
+
+document.querySelector('.number').textContent = 13;
+document.querySelector('.score').textContent = 10;
+
+document.querySelector('.guess').value = 1;
+console.log(document.querySelector('.guess').value);
