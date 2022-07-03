4d2390e9090b94632d9ab020256f2711810a1881
Dachi <dachi.laperashvili@gmail.com>
2022-07-03 14:08:06 +0400

    added logic to the code

--- 5 Guess my number/script.js
+++ 5 Guess my number/script.js
@@ -8,11 +8,33 @@ document.querySelector('.score').textContent = 10;
 document.querySelector('.guess').value = 1;
 */
 
+let secretNum = Math.trunc(Math.random() * 20) + 1;
+let message = document.querySelector('.message');
+let box = document.querySelector('.number');
+let score = 10;
+
 const guessNum = function () {
   const guess = Number(document.querySelector('.guess').value);
-  console.log(guess);
+
   if (!guess) {
-    document.querySelector('.message').textContent = '❌ Not a Number!';
+    message.textContent = '❌ Not a Number!';
+  } else if (guess === secretNum) {
+    message.textContent = '🎉 Correct Number!';
+    box.textContent = secretNum;
+  } else if (guess > secretNum) {
+    message.textContent = '📈 Too High!';
+    score--;
+    document.querySelector('.score').textContent = score;
+  } else if (guess < secretNum) {
+    message.textContent = '📉 Too Low!';
+    score--;
+    document.querySelector('.score').textContent = score;
+  }
+
+  if (score < 1) {
+    document.querySelector('h1').textContent = 'Game Over!';
+    message.textContent = 'You Lost The Game!';
+    document.querySelector('.score').textContent = 0;
   }
 };
 
