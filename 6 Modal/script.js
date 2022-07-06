86033d07a7b55b6bbbfe38efdfdd87798240c207
Dachi <dachi.laperashvili@gmail.com>
2022-07-06 10:15:29 +0400

    looped through buttons and logged their content

--- 6 Modal/script.js
+++ 6 Modal/script.js
@@ -1,1 +1,10 @@
 'use strict';
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnClose = document.querySelector('.close-modal');
+const btnsShow = document.querySelectorAll('.show-modal');
+
+console.log(btnsShow);
+
+for (let i = 0; i < btnsShow.length; i++) console.log(btnsShow[i].textContent);
