20dcced85e1662413ebc660f86577dba4eb0590a
Dachi <dachi.laperashvili@gmail.com>
2022-07-07 10:32:24 +0400

    selected elements and added starting conditions

--- 7 Pig Game/script.js
+++ 7 Pig Game/script.js
@@ -1,1 +1,21 @@
 'use strict';
+
+// Selecting elements
+const score0 = document.getElementById('score--0');
+const score1 = document.getElementById('score--1');
+const current0 = document.getElementById('current--0');
+const current1 = document.getElementById('current--1');
+
+const diceEl = document.querySelector('.dice');
+const btnRoll = document.querySelector('.btn--roll');
+const btnHold = document.querySelector('.btn--hold');
+const btnNew = document.querySelector('.btn--new');
+
+// Starting conditions
+score0.textContent = 0;
+score1.textContent = 0;
+diceEl.classList.add('hidden');
+
+const scores = [0, 0];
+let currentScore = 0;
+let activePlayer = 0;
